f46407360930b85ab51e408c4bf53b054a309a6e
Paurush Sinha <76876045+sinhapaurush@users.noreply.github.com>
2021-09-17 18:15:24 +0530

    Added $noscript
    Universal Noscript code can be edited here.

--- string.php
+++ string.php
@@ -19,7 +19,23 @@
 
     $site_title = "YUGAL SITE TITLE"; 
     //This will be used by Framework and external Library will be able to reach it.
-    
+    $yugal_noscript = "<div>
+        <h1>JAVASCRIPT ERROR</h1>
+        <p>Oops! Can't find JavaScript enabled in your browser, this site uses Javascript, please upgrade browser or enable Javascript.</p>
+    </div>
+    <style>
+        noscript div{
+            width: 100vw;
+    height: 100vh;
+    background: #f6e4e4;
+    color: red;
+    font-weight: bold;
+    position: fixed;
+    text-align:center;
+    padding-top: 20px
+        }
+    </style>
+    ";
     
     $webapp = false; //ENTER true if you are building webapp, else false. If you are not building webapp then you can delete 'menifest.webmenifest' file from dir.
     //TRUE IF YOU WANT TO USE SPA FEATURE IN IT.
